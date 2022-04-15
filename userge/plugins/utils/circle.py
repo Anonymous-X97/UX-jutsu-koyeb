56de7b8de838f16d20560318fddbf7b4f2f4a248
Ryuk <90273333+Anonymous-X97@users.noreply.github.com>
2022-04-15 18:10:26 +0530

    Update circle.py

--- userge/plugins/utils/circle.py
+++ userge/plugins/utils/circle.py
@@ -94,7 +94,7 @@ async def crop_vid(input_vid: str, final_path: str):
         if correct_aspect:
             crop_by = min(width_, height_)
             await runcmd(
-                f"""ffmpeg -i \'{input_vid}\' -filter:v \"crop=180:180\" {final_path}"""
+                f"""ffmpeg -i \'{input_vid}\' -filter:v \"crop=180:180:0:0:keep_aspect=1\" {final_path}"""
             )
             os.remove(input_vid)
         else:
